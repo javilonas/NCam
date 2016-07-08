23b53a0d0cfba5db6cf21da1b4dc7e8bca30b061
javilonas <admin@lonasdigital.com>
2016-07-08 10:49:19 +0200

    update readme

--- globals.h
+++ globals.h
@@ -361,7 +361,7 @@ typedef unsigned char uchar;
  *         constants
  * =========================== */
 #define CS_VERSION    "1.1"
-#define DATE_BUILD    "07-07-2016"
+#define DATE_BUILD    "08-07-2016"
 #ifndef CS_SVN_VERSION
 #   define CS_SVN_VERSION "stable"
 #endif
